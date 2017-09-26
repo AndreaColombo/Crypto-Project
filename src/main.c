a6fdab13d9cd5b37a86338f66baf1a4a0febd721
root <root@localhost.localdomain>
2017-09-26 04:44:34 -0400

    button

--- src/main.c
+++ src/main.c
@@ -1,25 +1,9 @@
 #include <stm32f401xe.h>
+#include <stdint.h>
+#include <stdio.h>
 #include "rijndael-alg-fst.h"
+#include "serial.h"
 
-static const u8 key[16] = {
-//  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-//  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
-    0xab, 0xf7, 0x15, 0x88, 0x09,0xcf, 0x4f, 0x3c
-};
-
-static const u8 pt[16] = {
-//  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-//  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-    'A','A','A','A','A','A','A','A',
-    'A','A','A','A','A','A','A','A'
-};
-
-static const u8 ct_expected[16] = {
-  0x6c,0x2a,0x3f,0xd9,0x3a,0xa7,0xb4,0x17,0xc5,0x43,0x8d,0x26,0xa4,0x61,0x95,0x19
-};
-
-u8 ct[16];
 
 /*
  * The green LED is connected to port A5,
@@ -28,7 +12,7 @@ u8 ct[16];
 #define LED_GPIO        GPIOA
 #define LED_PIN         5
 
-
+void wait_button(void);
 /**
  * Quick 'n' dirty delay
  *
@@ -39,45 +23,58 @@ static void delay(unsigned time) {
         for (volatile unsigned j=0; j<20000; j++);
 }
 
+static const u8 aes_test_user_key[16] = {
+    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+    0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+};
+static const u8 pt[16] = {
+'A','A','A','A','A','A','A','A',
+'A','A','A','A','A','A','A','A'
+};
+static const u8 expected_ct[16] = {
+0x6c,0x2a,0x3f,0xd9,0x3a,0xa7,0xb4,0x17,0xc5,0x43,0x8d,0x26,0xa4,0x61,0x95,0x19
+};
 
+u8 ct[16];
+u32 rk[128];
 /**
  * Hello world blinky program
  *
  * @return never
  */
 int main(void) {
-   int del;
-   u32 rk[128];
-   int nr = rijndaelKeySetupEnc(rk, key, 128);
-// rijndaelKeySetupDec(rk, key, 128);
-   rijndaelEncrypt(rk, nr, pt, ct);
-   int i;
-   int eq = 1;
-   for (i=0; i<16 && eq; i++){
-      if(ct[i]!=ct_expected[i])
-         eq=0;
-   }
-   
-   if (eq)
-      del = 20;
-   else del = 2000;   
-   
+
+    
+    int del=200;
+    
+    int nr=rijndaelKeySetupEnc(rk, aes_test_user_key,128);
+    rijndaelEncrypt(rk, nr, pt, ct);
+
+    int cnt=1;
+    for(int i=0;i<16 && cnt;i++)
+        if(ct[i]!=expected_ct[i])
+           cnt=0;
     /*
      * Turn on the GPIOA unit,
      * -> see section 6.3.9 in the manual
      */
     RCC->AHB1ENR  |= RCC_AHB1ENR_GPIOAEN;
-
-
+    //int j=serialPort();
+    
     /*
      * Set LED-Pin as output
      * Note: For simplicity this assumes the pin was configured
      * as input before, as it is when it comes out of reset.
      * -> see section 8.4.1 in the manual
      */
+    LED_GPIO->MODER=0;
     LED_GPIO->MODER |= (0b01 << (LED_PIN << 1));
-
-
+    
+    if(cnt)
+       del=200;
+    else
+        del=2000;
+    //wait_button();
     while(1) {
 
         /*
@@ -89,7 +86,7 @@ int main(void) {
         LED_GPIO->BSRR = (1 << LED_PIN);
 
         delay(del);
-
+        //write("criptandoooooo\r\n");
         /*
          * LED off (drive the pin low)
          * A GPIO output pin is set to low by writing 1 to the
@@ -100,5 +97,9 @@ int main(void) {
 
         delay(del);
     }
+}
+
+void wait_button(void) {
+  while (!((LED_GPIO->IDR) & (1<<0))) {}
 }
 
